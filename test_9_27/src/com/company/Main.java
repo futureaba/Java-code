a3346244458ba71724d8d05512487bf2d262e920
Veronica <3197898005@qq.com>
2021-10-03 14:09:57 +0800

    测试代码

---
+++ test_9_27/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*System.out.println(5/2);
+        System.out.println(5.0/2);
+        System.out.println((float)5/2);//将5进行了强制类型的转换
+        System.out.println((float)(5/2));//将5/2的结果进行了强制类型的转换
+        */
+     /* System.out.println(10%3);
+        System.out.println(-10%3);
+        System.out.println(10%-3);
+        System.out.println(-10%-3);
+
+      */
+       /* int a =10;
+        int b =20;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        */
+
+    }
+}
